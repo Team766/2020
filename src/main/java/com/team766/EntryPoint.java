37a6e0a50645da4ee0cbe7269b15c3c5b99fe630
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2019-01-07 03:54:17 -0800

    Convert to 2019 wpilib project structure

---
+++ src/main/java/com/team766/EntryPoint.java
@@ -0,0 +1,13 @@
+package com.team766;
+
+import com.team766.hal.MyRobot;
+
+public class EntryPoint {
+    public static MyRobot createRobot() {
+        return new com.team766.frc2019.Robot();
+    }
+
+    public static String getSimConfigFile() {
+        return com.team766.frc2019.SimConfig.getSimConfigFile();
+    }
+}
---
+++ src/main/java/com/team766/hal/wpilib/RobotMain.java
@@ -0,0 +1,96 @@
+package com.team766.hal.wpilib;
+
+import com.team766.EntryPoint;
+import com.team766.config.ConfigFileReader;
+import com.team766.framework.Scheduler;
+import com.team766.hal.MyRobot;
+import com.team766.hal.RobotProvider;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class RobotMain extends TimedRobot {
+	private MyRobot robot;
+
+	public static void main(String... args) {
+		RobotBase.startRobot(RobotMain::new);
+	}
+
+	@Override
+	public void robotInit() {
+		try{
+			ConfigFileReader.instance = new ConfigFileReader("/home/lvuser/robotConfig.txt");
+			RobotProvider.instance = new WPIRobotProvider();
+			
+			robot = EntryPoint.createRobot();
+			
+			robot.robotInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void disabledInit() {
+		try{
+			robot.disabledInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	@Override
+	public void autonomousInit() {
+		try{
+			robot.autonomousInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void teleopInit() {
+		try{
+			robot.teleopInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		try{
+			robot.disabledPeriodic();
+			Scheduler.getInstance().run();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		try{
+			robot.autonomousPeriodic();
+			Scheduler.getInstance().run();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		try{
+			robot.teleopPeriodic();
+			Scheduler.getInstance().run();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}
