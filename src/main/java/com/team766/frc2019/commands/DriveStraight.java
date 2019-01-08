9a3e4aa76d900342e2d7c2b4b0a925036e12adc8
AJ <alexander.j.smyth@gmail.com>
2019-01-08 09:25:18 -0800

    Add in tutorial code

---
+++ src/main/java/com/team766/frc2019/commands/DriveStraight.java
@@ -0,0 +1,23 @@
+package com.team766.frc2019.commands;
+
+import com.team766.framework.Subroutine;
+import com.team766.frc2019.Robot;
+
+public class DriveStraight extends Subroutine {
+    public DriveStraight() {
+        takeControl(Robot.drive);
+    }
+    
+    protected void subroutine() {
+        Robot.drive.setDrivePower(1.0,1.0);
+        new java.util.Timer().schedule( 
+        new java.util.TimerTask() {
+            @Override
+            public void run() {
+                Robot.drive.setDrivePower(0.0,0.0);
+            }
+        }, 
+        5000 
+        );
+    }
+}
