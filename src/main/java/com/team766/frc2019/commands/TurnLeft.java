8cf48c857d2174058133e97afc0f5ea479b7413c
Alexander <arghalexander3000@gmail.com>
2019-01-11 20:30:33 -0800

    everything fixed now
    it also doesnt kill the robot

--- src/main/java/com/team766/frc2019/commands/TurnLeft.java
+++ src/main/java/com/team766/frc2019/commands/TurnLeft.java
@@ -5,16 +5,22 @@ import com.team766.frc2019.Robot;
 
 public class TurnLeft extends Subroutine {
 
-    double m_turnTime;
+    /*double m_turnTime;
 
     public TurnLeft(double turnTime) {
         m_turnTime = turnTime;
         takeControl(Robot.drive);
+    }*/
+
+    public TurnLeft() {
+        takeControl(Robot.drive);
     }
 
+
     protected void subroutine() {
-        Robot.drive.setDrivePower(1.0, 1.0);
-        waitForSeconds(m_turnTime);
+        Robot.drive.setDrivePower(0.25, 0.25);
+        //waitForSeconds(m_turnTime);
+        waitForSeconds(1.0);
 
         Robot.drive.setDrivePower(0.0, 0.0);
     }
