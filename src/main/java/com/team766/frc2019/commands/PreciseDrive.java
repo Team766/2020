4258e5d9ba9c2536047f1e33ee2768e87f9b1000
Alexander <arghalexander3000@gmail.com>
2019-01-14 20:43:26 -0800

    fixes to corrections

--- src/main/java/com/team766/frc2019/commands/PreciseDrive.java
+++ src/main/java/com/team766/frc2019/commands/PreciseDrive.java
@@ -21,17 +21,33 @@ public class PreciseDrive extends Subroutine {
         double leftPower;
         double rightPower;
         int i = 0;
-        int numUpdates = 1000;
-        double currentAngle = Robot.drive.getGyroAngle();
+        int numUpdates = 10;
+        double targetAngle = Robot.drive.getGyroAngle();
         double error = 0;
-        while (i < m_driveTime * numUpdates) {
+        while (i < (m_driveTime * numUpdates)) {
             leftPower = 0.25;
             rightPower = 0.25;
-            if (Math.round(Robot.drive.getGyroAngle()) != Math.round(currentAngle)) {
-                error = Robot.drive.getGyroAngle() - currentAngle;
-                rightPower *= Math.pow((error / 180), 2) + 1;
-                leftPower *= Math.pow((error / 180) - 1, 2) + 1;
+            if (Math.round(Robot.drive.getGyroAngle()) != Math.round(targetAngle)) {
+                if (Robot.drive.getGyroAngle() < targetAngle) {
+                    if (Math.abs(Robot.drive.getGyroAngle() - targetAngle) < 180) {
+                        error = Math.abs(Robot.drive.getGyroAngle() - targetAngle);
+                    } else {
+                        error = -Math.abs(Robot.drive.getGyroAngle() - targetAngle);
+                    }
+                } else {
+                    if (Math.abs(Robot.drive.getGyroAngle() - targetAngle) < 180) {
+                        error = -Math.abs(Robot.drive.getGyroAngle() - targetAngle);
+                    } else {
+                        error = Math.abs(Robot.drive.getGyroAngle() - targetAngle);
+                    }
+                }
+                if (error > 0) {
+                    rightPower *= Math.pow((error / 180), 2) + 1;
+                } else {
+                    leftPower *= Math.pow((error / 180), 2) + 1;
+                }
             }
+            System.out.println("(Going Straight) Current Angle: " + Robot.drive.getGyroAngle() + " Target Angle: " + targetAngle + " Left Power: " + leftPower + " Right Power: " + rightPower);
             i++;
             Robot.drive.setDrivePower(leftPower, rightPower);
             waitForSeconds(1.0 / numUpdates);
