a7578e2d2668434316176351a7bf5d34d5e12de5
daiki <daiki.takeuchi.02@gmaiol.com>
2019-01-19 11:55:52 -0800

    Update Drive.java

--- src/main/java/com/team766/mechanism/Drive.java
+++ src/main/java/com/team766/mechanism/Drive.java
@@ -1,1 +1,23 @@
 package com.team766.mechanism;
+
+import com.team766.framework.Mechanism;
+import com.team766.hal.RobotProvider;
+import com.team766.hal.SpeedController;
+
+public class Drive extends Mechanism {
+
+    private SpeedController m_rightMotor;
+    private SpeedController m_leftMotor;
+
+    public Drive() {
+        m_leftMotor = RobotProvider.instance.getMotor("drive.leftMotor");
+        m_rightMotor = RobotProvider.instance.getMotor("drive.rightMotor");
+    }
+
+    public void setDrivePower(double leftPower, double rightPower) {
+        m_leftMotor.set(leftPower);
+        m_rightMotor.set(rightPower);
+    }
+}
+
+
