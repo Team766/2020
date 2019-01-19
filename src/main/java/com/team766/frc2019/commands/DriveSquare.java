f41f65d3c86670aa1fc55b01e6cbf8c20efb7d0b
YardenGoraly <796435@seq.org>
2019-01-19 15:49:07 -0800

    Fixing DriveSquare losing control
    Fixed the scheduler and stuff with Ryan. Continuous commands now work. PID TBD in next release w/ Samir+Aidan.

--- src/main/java/com/team766/frc2019/commands/DriveSquare.java
+++ src/main/java/com/team766/frc2019/commands/DriveSquare.java
@@ -2,25 +2,29 @@ package com.team766.frc2019.commands;
 
 import com.team766.framework.Subroutine;
 import com.team766.frc2019.Robot;
-import com.team766.hal.RobotProvider;
+//import com.team766.hal.RobotProvider;
 import java.util.Date;
 
 public class DriveSquare extends Subroutine {
     public DriveSquare() {
-        takeControl(Robot.drive);
+        //takeControl(Robot.drive);
     }
 
     protected void subroutine() {
         double startTime = new Date().getTime();
         System.out.println("DRIVESQUARE STARTING");
-        callSubroutine(new PreciseTurn(-20)); 
+
+        for (int i=1; i<5; i++) {
+            System.out.println("Entering DriveSquare loop # " + i);
+            callSubroutine(new DriveStraight(1)); 
+            callSubroutine(new PreciseTurn(-90));
+        }
+
         System.out.println("DRIVESQUARE IS DONE");
-        //callSubroutine(new PreciseTurn(90));
+
 
         double endTime = new Date().getTime();
-        System.out.println("ENDED IN" + (endTime - startTime));
-        // callSubroutine(new PreciseTurn(45)); 
-
+        System.out.println("ENDED IN " + (endTime - startTime));
 
     }  
 }
