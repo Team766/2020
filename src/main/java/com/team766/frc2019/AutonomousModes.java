334bcde729077f3af0d5408c5aa03145a74c0d31
daiki <daiki.takeuchi.02@gmaiol.com>
2019-01-19 11:56:27 -0800

    Update AutonomousModes.java

--- src/main/java/com/team766/frc2019/AutonomousModes.java
+++ src/main/java/com/team766/frc2019/AutonomousModes.java
@@ -3,4 +3,5 @@ package com.team766.frc2019;
 import com.team766.framework.AutonomousCommand;
 import com.team766.frc2019.commands.DriveStraight;
 public enum AutonomousModes {
+    @AutonomousCommand(commandClass= DriveStraight.class)
 }
