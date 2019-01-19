be488475fbe3a56f27d07ff50ab2aa5da0d914d9
daiki <daiki.takeuchi.02@gmaiol.com>
2019-01-19 11:55:08 -0800

    Update DriveSquare.java

--- src/main/java/com/team766/frc2019/commands/DriveSquare.java
+++ src/main/java/com/team766/frc2019/commands/DriveSquare.java
@@ -1,1 +1,24 @@
+package com.team766.frc2019.commands;
+
+import com.team766.framework.Subroutine;
+
+public class DriveSquare extends Subroutine {
+    protected void subroutine(){
+        //first side
+        callSubroutine(new DriveStraight());
+        //first corner
+        callSubroutine(new TurnLeft());
+        //second side
+        callSubroutine(new DriveStraight());
+        //second corner
+        callSubroutine(new TurnLeft());
+        //third side
+        callSubroutine(new DriveStraight());
         //third corner
+        callSubroutine(new TurnLeft());
+        //fourth side
+        callSubroutine(new DriveStraight());
+        //fourth corner
+        callSubroutine(new TurnLeft());
+    }
+}
