9e205782a220be496fcc445b9c11e8ee9c5afbd3
daiki <daiki.takeuchi.02@gmaiol.com>
2019-01-19 16:01:47 -0800

    Update AutonomousModes.java

--- src/main/java/com/team766/frc2019/AutonomousModes.java
+++ src/main/java/com/team766/frc2019/AutonomousModes.java
@@ -3,7 +3,6 @@ package com.team766.frc2019;
 import com.team766.framework.AutonomousCommand;
 import com.team766.frc2019.commands.DriveStraight;
 import com.team766.frc2019.commands.DriveSquare;
-import com.team766.frc2019.commands.TurnLeft;;
 import com.team766.frc2019.commands.TurnLeft;
 import com.team766.frc2019.commands.ExtendGripper;
 import com.team766.frc2019.commands.RetractGripper;
