65fcdb85ca3d371a18e5f24876cf083841487331
Alexander <arghalexander3000@gmail.com>
2019-02-25 18:28:12 -0800

    despacito
    frick u bora code lol

--- src/main/java/com/team766/frc2019/mechanisms/Drive.java
+++ src/main/java/com/team766/frc2019/mechanisms/Drive.java
@@ -6,8 +6,13 @@ import com.team766.hal.CANSpeedController;
 import com.team766.hal.EncoderReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.CANSpeedController.ControlMode;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
 import com.team766.controllers.PIDController;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.sun.security.auth.login.ConfigFile;
 import com.team766.config.ConfigFileReader;
 
 
@@ -21,23 +26,26 @@ public class Drive extends Mechanism {
     private CANSpeedController m_leftTalon;
     private CANSpeedController m_rightTalon;
     private GyroReader m_gyro;
-    private EncoderReader m_leftEncoder; 
-    private EncoderReader m_rightEncoder;
     public static double P = 0.04;
-    public static double I = 0.0;
+    public static double I = 0.001;
     public static double D = 0.004;
+    public static double MP = 0.05;
+    public static double MI = 0.01;
+    public static double MD = 0.01;
     public static double THRESHOLD = 3;
     public static double MAX_TURN_SPEED = 0.75;
     public static double MIN_TURN_SPEED = 0.1;
-    public static double DIST_PER_PULSE = 0.00159616132;
+    public static double DIST_PER_PULSE = ConfigFileReader.getInstance().getDouble("drive.DIST_PER_PULSE").get();
+    public static double POSITION_PER_INCH = 20000;
     public static double robotWidth = 2.8;
     public static boolean m_secondVictor = true;
+    public double m_gyroDirection = 1.0;
     
 
     public Drive() { 
         m_leftVictor1 = RobotProvider.instance.getVictorCANMotor("drive.leftVictor1"); 
         m_rightVictor1 = RobotProvider.instance.getVictorCANMotor("drive.rightVictor1");
-        if ( ConfigFileReader.getInstance().getInt("drive.leftVictor2").get() >= 0) {
+        if (ConfigFileReader.getInstance().getInt("drive.leftVictor2").get() >= 0) {
             m_secondVictor = true;
             m_leftVictor2 = RobotProvider.instance.getVictorCANMotor("drive.leftVictor2");
             m_rightVictor2 = RobotProvider.instance.getVictorCANMotor("drive.rightVictor2");
@@ -46,15 +54,24 @@ public class Drive extends Mechanism {
         }
         m_leftTalon = RobotProvider.instance.getTalonCANMotor("drive.leftTalon");
         m_rightTalon = RobotProvider.instance.getTalonCANMotor("drive.rightTalon");
-        m_leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
-        m_rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
+        
+        //m_leftEncoder = RobotProvider.instance.getEncoder("drive.leftEncoder");
+        //m_rightEncoder = RobotProvider.instance.getEncoder("drive.rightEncoder");
         m_gyro = RobotProvider.instance.getGyro("drive.gyro");
+        m_leftTalon.configFactoryDefault();
+        m_rightTalon.configFactoryDefault();
+        m_leftTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        m_rightTalon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
         m_rightTalon.setInverted(true);
         m_rightVictor1.setInverted(true);
         if (m_secondVictor) {
             m_rightVictor2.setInverted(true);
         }
+<<<<<<< HEAD
         // left false right true for kylo, both false for viper and marie
+=======
+        // left false right true for new, both false for mule
+>>>>>>> parent of 5e74556... ok
         m_leftTalon.setSensorPhase(false);
         m_rightTalon.setSensorPhase(false);
         m_leftTalon.configNominalOutputForward(0);
@@ -65,15 +82,29 @@ public class Drive extends Mechanism {
         m_rightTalon.configNominalOutputReverse(0);
         m_rightTalon.configPeakOutputForward(1);
         m_rightTalon.configPeakOutputReverse(-1);
+<<<<<<< HEAD
         m_leftTalon.config_kP(0, P, 0);
         m_leftTalon.config_kI(0, I, 0);
         m_leftTalon.config_kD(0, D, 0);
         m_rightTalon.config_kP(0, P, 0);
         m_rightTalon.config_kI(0, I, 0);
         m_rightTalon.config_kD(0, P, 0);
+=======
+        m_leftTalon.config_kP(0, MP, 0);
+        m_leftTalon.config_kI(0, MI, 0);
+        m_leftTalon.config_kD(0, MD, 0);
+        m_rightTalon.config_kP(0, MP, 0);
+        m_rightTalon.config_kI(0, MI, 0);
+        m_rightTalon.config_kD(0, MP, 0);
+>>>>>>> parent of 5e74556... ok
         m_leftTalon.setNeutralMode(NeutralMode.Brake);
         m_rightTalon.setNeutralMode(NeutralMode.Brake);
-        encodersDistancePerPulse(DIST_PER_PULSE);
+        m_leftTalon.configOpenLoopRamp(0.5, 0);
+        m_leftTalon.configClosedLoopRamp(0.5, 0);
+        m_rightTalon.configOpenLoopRamp(0.5, 0);
+        m_rightTalon.configClosedLoopRamp(0.5, 0);
+        //encodersDistancePerPulse(DIST_PER_PULSE);
+        m_gyroDirection = ConfigFileReader.getInstance().getDouble("drive.gyroDirection").get();
     }
 
     /**
@@ -89,50 +120,55 @@ public class Drive extends Mechanism {
             m_leftVictor2.follow(m_leftTalon);
             m_rightVictor2.follow(m_rightTalon);
         }
+        /*m_leftVictor1.setNeutralMode(NeutralMode.Coast);
+        m_leftVictor2.setNeutralMode(NeutralMode.Coast);
+        m_rightVictor1.setNeutralMode(NeutralMode.Coast);
+        m_rightVictor2.setNeutralMode(NeutralMode.Coast);*/
+    }
+
+    public boolean isEnabled() {
+        return(DriverStation.getInstance().isEnabled());
     }
 
     public double getGyroAngle() {
-        return(m_gyro.getAngle());
+        return((m_gyro.getAngle() % 360) * m_gyroDirection);
     }
 
     public void resetGyro() {
         m_gyro.reset(); 
     }
 
-    public double leftEncoderDistance() { 
+    public double leftEncoderDistance() {
         return(m_leftTalon.getSensorPosition());
     }
 
-    public double rightEncoderDistance() { 
+    public double rightEncoderDistance() {
         return(m_rightTalon.getSensorPosition());
-    }
-
-    public void setDrivePower(double leftPower, double rightPower, ControlMode controlMode) {
-        m_leftTalon.set(controlMode, leftPower);
-        m_rightTalon.set(controlMode, rightPower);
     }
 
     /**
     * Returns the object of the specified encoder.
     * turnDirection = true returns the left encoder, and false returns the right encoder.
     */
-    public EncoderReader getOutsideEncoder(boolean turnDirection) {
+    public double getOutsideEncoderDistance(boolean turnDirection) {
         if (turnDirection) {
-            return(m_leftEncoder);
+            return(leftEncoderDistance());
         } else {
-            return(m_rightEncoder);
+            return(rightEncoderDistance());
         }
     }
 
     public void resetEncoders() {
         m_leftTalon.setPosition(0);
         m_rightTalon.setPosition(0);
+        //m_leftEncoder.reset();
+        //m_rightEncoder.reset();
     }
 
-    public void encodersDistancePerPulse(double distancePerPulse) {
+    /*public void encodersDistancePerPulse(double distancePerPulse) {
         m_leftEncoder.setDistancePerPulse(distancePerPulse);
         m_rightEncoder.setDistancePerPulse(distancePerPulse);
-    }
+    }*/
 
     public void shutdown() {
         m_leftTalon.set(ControlMode.PercentOutput, 0);
@@ -140,12 +176,6 @@ public class Drive extends Mechanism {
         m_leftTalon.setNeutralMode(NeutralMode.Coast);
         m_rightTalon.setNeutralMode(NeutralMode.Coast);
     }
-
-    /*@Override
-    public void run() {
-        setDrivePower(leftPower, rightPower);
-    }
-    */
     
     public boolean isTurnDone(PIDController turnController) {
         if (turnController == null) {
@@ -158,8 +188,15 @@ public class Drive extends Mechanism {
 	 * Gets the difference between angle1 and angle2, between -180 and 180 degrees.
      * angle1 is the current angle, angle2 is the desired angle.
 	 */
-    public double AngleDifference(double angle1, double angle2) {
+    public double AngleDifference(double angle1, double angle2) { //356, 0
         double diff = (angle2 - angle1 + 180) % 360 - 180;
         return diff < -180 ? diff + 360 : diff;
+        //return diff;
+    }
+
+    public void nukeRobot() {
+        shutdown();
+        resetEncoders();
+        resetGyro();
     }
 }
--- src/main/java/com/team766/frc2019/mechanisms/Elevator.java
+++ src/main/java/com/team766/frc2019/mechanisms/Elevator.java
@@ -1,41 +1,359 @@
 package com.team766.frc2019.mechanisms;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.team766.framework.Mechanism;
 import com.team766.frc2019.Robot;
 import com.team766.hal.CANSpeedController;
+import com.team766.hal.DigitalInputReader;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.CANSpeedController.ControlMode;
 
 
+
 public class Elevator extends Mechanism {
 
-    private CANSpeedController m_elevatorMotor;
-    private CANSpeedController m_actuatorMotor;
+    private CANSpeedController m_lowerElevatorMotor;
+    private CANSpeedController m_upperElevatorMotor;
+    private DigitalInputReader lowerElevatorMinLimitSwitch;
+    private DigitalInputReader lowerElevatorMaxLimitSwitch;
+    private DigitalInputReader upperElevatorMinLimitSwitch;
+    private DigitalInputReader upperElevatorMaxLimitSwitch;
     public static double DIST_PER_PULSE = Robot.drive.DIST_PER_PULSE;
+    public static double targetPosition;
+
+    public static int LVL1 = 100000;
+    public static int LVL2 = 1480000;
+    public static int LVL3 = 3000000;
+    public static int MIN_LOWER_HEIGHT = 40000;
+    public static int VERY_CLOSE_MIN_LOWER_HEIGHT = 100000;
+	public static int NEAR_MIN_LOWER_HEIGHT = 400000;
+    private static int NEAR_MAX_LOWER_HEIGHT = 1800000;
+    private static int MAX_LOWER_HEIGHT = 2130000;
+	public static int MIN_UPPER_HEIGHT = 0;
+    public static int NEAR_MIN_UPPER_HEIGHT = 200000;
+    private static int NEAR_MAX_UPPER_HEIGHT = 880000;
+    private static int MAX_UPPER_HEIGHT = 920000;
+    private static int MID_HEIGHT_BIG = 1000000;
+	private static int MAX_HEIGHT_BIG = 1930000;
+	private static int MID_HEIGHT_SMALL = 500000;
+    private static int MAX_HEIGHT_SMALL = 900000;
+    public static int MAX_COMBINED_HEIGHT = MAX_LOWER_HEIGHT + MAX_UPPER_HEIGHT;
+    
+    private boolean setPositionRunning = false;
+    public static boolean combinedStopTargeting = false;
+    public static boolean upperStopTargeting = false;
+    public static boolean hoverAtZero = false;
+    private int index = 0;
 
     public Elevator() {
-        m_elevatorMotor = RobotProvider.instance.getTalonCANMotor("elevator.elevatorMotor");
-        m_actuatorMotor = RobotProvider.instance.getTalonCANMotor("elevator.actuatorMotor"); 
+        m_lowerElevatorMotor = RobotProvider.instance.getTalonCANMotor("elevator.elevatorMotor");
+        m_upperElevatorMotor = RobotProvider.instance.getTalonCANMotor("elevator.actuatorMotor");
+        lowerElevatorMinLimitSwitch = RobotProvider.instance.getDigitalInput("elevator.lowerMinLimitSwitch");
+        lowerElevatorMaxLimitSwitch = RobotProvider.instance.getDigitalInput("elevator.lowerMaxLimitSwitch");
+        upperElevatorMinLimitSwitch = RobotProvider.instance.getDigitalInput("elevator.upperMinLimitSwitch");
+        upperElevatorMaxLimitSwitch = RobotProvider.instance.getDigitalInput("elevator.upperMaxLimitSwitch");
+        m_lowerElevatorMotor.configFactoryDefault();
+        m_upperElevatorMotor.configFactoryDefault();
+        m_upperElevatorMotor.setInverted(true);
+        m_lowerElevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        m_upperElevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        m_lowerElevatorMotor.setSensorPhase(true);
+        m_upperElevatorMotor.setSensorPhase(false);
+        m_lowerElevatorMotor.configNominalOutputForward(0.0);
+        m_lowerElevatorMotor.configNominalOutputReverse(0.0);
+        m_lowerElevatorMotor.configPeakOutputForward(1.0);
+        m_lowerElevatorMotor.configPeakOutputReverse(-1.0);
+        m_upperElevatorMotor.configNominalOutputForward(0.0);
+        m_upperElevatorMotor.configNominalOutputReverse(0.0);
+        m_upperElevatorMotor.configPeakOutputForward(1.0);
+        m_upperElevatorMotor.configPeakOutputReverse(-1.0);
+        m_lowerElevatorMotor.config_kP(0, 0.01, 0);
+        m_lowerElevatorMotor.config_kI(0, 0.0, 0);
+        m_lowerElevatorMotor.config_kD(0, 0.01, 0);
+        m_lowerElevatorMotor.setFeedForward(0.0);
+        m_upperElevatorMotor.config_kP(0, 0.01, 0);
+        m_upperElevatorMotor.config_kI(0, 0.0, 0);
+        m_upperElevatorMotor.config_kD(0, 0.01, 0);
+        m_upperElevatorMotor.setFeedForward(0.0);
+        m_lowerElevatorMotor.setNeutralMode(NeutralMode.Brake);
+        m_upperElevatorMotor.setNeutralMode(NeutralMode.Brake);
+        m_upperElevatorMotor.setPosition(0);
+        m_lowerElevatorMotor.setPosition(0);
     }
 
-    public void setElevatorPower(double elevatorPower) {
-        m_elevatorMotor.set(ControlMode.PercentOutput, elevatorPower);
+    // Limit switch code
+    
+    public void hover() {
+        Robot.elevator.setLowerPosition(Math.max(Robot.elevator.getLowerHeight(),0.0));
     }
-    public void setActuatorPower(double actuatorPower) {
-        m_actuatorMotor.set(ControlMode.PercentOutput, actuatorPower);
+
+    public boolean getLowerMinLimitSwitch() {
+        return lowerElevatorMinLimitSwitch.get();
     }
+
+    public boolean getLowerMaxLimitSwitch() {
+        return lowerElevatorMaxLimitSwitch.get();
+    }
+
+    public boolean getUpperMinLimitSwitch() {
+        return upperElevatorMinLimitSwitch.get();
+    }
+
+    public boolean getUpperMaxLimitSwitch() {
+        return upperElevatorMaxLimitSwitch.get();
+    }
+
+
+    public void resetUpperEncoder() {
+        m_upperElevatorMotor.setPosition(0);
+    }
+
+    public void resetLowerEncoder() {
+        m_lowerElevatorMotor.setPosition(0);
+    }
+
+    public void setLowerPower(double elevatorPower) {
+        //System.out.println("*** setting lower power to " + elevatorPower + " ***");
+        m_lowerElevatorMotor.set(ControlMode.PercentOutput, elevatorPower);
+    }
+
+    public void setUpperPower(double actuatorPower) {
+        //System.out.println("*** setting upper power to " + actuatorPower + " ***");
+        m_upperElevatorMotor.set(ControlMode.PercentOutput, actuatorPower);
+    }
+
+    public void setLowerPosition(double position) {
+        System.out.println("*** setting lower position to " + position + " ***");
+        m_lowerElevatorMotor.set(ControlMode.Position, position);
+    }
+    
+    public void setUpperPosition(double position) {
+        System.out.println("*** setting upper position to " + position + " ***");
+        m_upperElevatorMotor.set(ControlMode.Position, position);
+    }
+    
+    public void setCombinedPosition(double position) {
+        if (setPositionRunning) {
+           return;
+        }
+        setPositionRunning = true;
+        final double upperTarget = 92*position/305;
+        final double lowerTarget = 213*position/305;
+        System.out.println("Setting position to: " + position + ", target (U, L): " +upperTarget + ", " + lowerTarget + ", combinedStopTargeting: " + combinedStopTargeting );
+        if (position > 0 && position < (double)(MAX_LOWER_HEIGHT + MAX_UPPER_HEIGHT) && combinedStopTargeting == false) {
+            if (position < NEAR_MIN_LOWER_HEIGHT) {
+                System.out.println( "Getting elevator to lowest position ");
+                // If lower nears bottom, slow down
+                // As long as either upper or lower elevators are higher than their target, lower them down
+                boolean upperDone = false;
+                boolean lowerDone = false;
+                do {
+                    //upper
+                    if( !upperDone ){
+                        if (getUpperHeight() <= MIN_UPPER_HEIGHT || !getUpperMinLimitSwitch()) {
+                            setUpperPower(0.0);
+                            upperDone = true;
+                            System.out.println("Upper is done");
+                        } else if (getUpperHeight() <= NEAR_MIN_UPPER_HEIGHT) {
+                            setUpperPower(-0.4);
+                        } else {
+                            setUpperPower(-1.0);
+                        }    
+                    }
+                    //lower
+                    if ( !lowerDone ) {
+                        if (getLowerHeight() <= MIN_LOWER_HEIGHT || !getLowerMinLimitSwitch()) {
+                            setLowerPower(0.0);
+                            lowerDone = true;
+                            System.out.println("Lower is done");
+                        } else if (getLowerHeight() <= NEAR_MIN_LOWER_HEIGHT) {
+                            setLowerPower(-0.4);
+                        } else {
+                            setLowerPower(-1.0);
+                        }
+                    }                 
+                }
+                while( !upperDone || !lowerDone );
+
+                System.out.println("Elevator should be down");
+/*
+                while ((Robot.elevator.getUpperHeight() > upperTarget) && 
+                                (Robot.elevator.getLowerHeight() > lowerTarget || getLowerMinLimitSwitch())){
+                    if (Robot.elevator.getUpperHeight() <= upperTarget) {
+                        Robot.elevator.setUpperPower(0.0);
+                    } else if (Robot.elevator.getUpperHeight() <= NEAR_MIN_UPPER_HEIGHT) {
+                            Robot.elevator.setUpperPower(-0.4);
+                    } else {
+                        Robot.elevator.setUpperPower(-1.0);
+                    }                    
+                    if (Robot.elevator.getLowerHeight() <= lowerTarget || !getLowerMinLimitSwitch()) {
+                        Robot.elevator.setLowerPower(0.0);
+                        hover();
+                    } else if (Robot.elevator.getLowerHeight() < NEAR_MIN_LOWER_HEIGHT) {
+     //                   System.out.println("Nearing Bottom");
+                        Robot.elevator.setLowerPower(-0.1);
+                    }  else {
+                        Robot.elevator.setLowerPower(-0.9);
+                    }
+                }
+                */
+            } else {
+                System.out.println("TARGETPOSITION: " + targetPosition + " upperTarget: " + upperTarget + " lowerTarget: " + lowerTarget);
+                setLowerPosition(lowerTarget);
+                setUpperPosition(upperTarget);
+                targetPosition = position;
+            }
+        } else {
+            System.out.println("Cannot reach target position");
+        }
+        setPositionRunning = false;
+    }
+
+    // Adds to DESTINATION, not current position. If robot is not moving to a LVL already, simply adds to current position.
+    public void addToPosition(double add) {
+        if (combinedStopTargeting == true) {
+            targetPosition = getLowerHeight() + getUpperHeight();
+            combinedStopTargeting = false;
+        }
+        if ((targetPosition + add) <= MAX_COMBINED_HEIGHT) {
+            targetPosition += add;
+            m_lowerElevatorMotor.set(ControlMode.Position, targetPosition);
+        }
+    }
+
+    public double getUpperHeight() {
+       return m_upperElevatorMotor.getSensorPosition();
+    }
+
+    public double getLowerHeight() {
+       return m_lowerElevatorMotor.getSensorPosition();
+    }
+
+
+    // less complicated method of setting elevator to destination
+    public void setLowerHeight(double position, double power) {
+        while (getLowerHeight() != position) {
+            if (getLowerHeight() > position) {
+                setLowerPower(-power);
+            } else {
+                setLowerPower(power);
+            }
+        }
+    }
+
+    public void setUpperHeight(double position, double power) {
+        while (getUpperHeight() != position) {
+            if (getUpperHeight() > position) {
+                setUpperPower(-power);
+            } else {
+                setUpperPower(power);
+            }
+        }
+    }
+
+    public void elevatorUp() {
+        combinedStopTargeting = true;
+        if (index++ % 2000 == 0 && Robot.drive.isEnabled()) {
+            System.out.println("LH: " + Robot.elevator.getLowerHeight() + " UH: " + Robot.elevator.getUpperHeight());
+        }
+        if (Robot.elevator.getLowerHeight() > NEAR_MAX_LOWER_HEIGHT) {
+            if (Robot.elevator.getLowerHeight() >= MAX_LOWER_HEIGHT) {
+                Robot.elevator.setLowerPower(0.0);
+                hover();
+            } else {
+                Robot.elevator.setLowerPower(0.6);
+            }
+            if (Robot.elevator.getUpperHeight() >= MAX_UPPER_HEIGHT) {
+                Robot.elevator.setUpperPower(0.0);
+            } else if (Robot.elevator.getUpperHeight() > NEAR_MAX_UPPER_HEIGHT) {
+    //            System.out.println("UPPER NEARING DESTINATION");
+                Robot.elevator.setUpperPower(0.6);
+            } else {
+                Robot.elevator.setUpperPower(1.0);
+            }
+        } else {
+            Robot.elevator.setLowerPower(1.0);
+        }
+    }
+
+    public void elevatorDown() {
+        combinedStopTargeting = true;
+        if (index++ % 2000 == 0 && Robot.drive.isEnabled()) {
+            System.out.println("LH: " + Robot.elevator.getLowerHeight() + " UH: " + Robot.elevator.getUpperHeight());
+        }
+        if (Robot.elevator.getUpperHeight() <= MIN_UPPER_HEIGHT) {
+            if (Robot.elevator.getLowerHeight() <= MIN_LOWER_HEIGHT || !getLowerMinLimitSwitch()) {
+                Robot.elevator.setLowerPower(0.0);
+                hover();
+            } else if (Robot.elevator.getLowerHeight() < NEAR_MIN_LOWER_HEIGHT) {
+    //            System.out.println("Nearing Bottom");
+                Robot.elevator.setLowerPower(-0.2);
+            }  else {
+                Robot.elevator.setLowerPower(-0.9);
+            }
+            Robot.elevator.setUpperPower(0.0);
+        } else if (Robot.elevator.getUpperHeight() <= NEAR_MIN_UPPER_HEIGHT) {
+                Robot.elevator.setUpperPower(-0.4);
+        } else {
+            Robot.elevator.setUpperPower(-1.0);
+        }
+    }
+
+    public void elevatorNeutral() {
+        if (combinedStopTargeting == true) {
+            System.out.println("Setting elevator to neutral");
+            Robot.elevator.setUpperPower(0.0);
+            hover();
+            targetPosition = getLowerHeight() + getUpperHeight();
+            combinedStopTargeting = false;
+        }
+    }
+
+    // Replacement for LVL1, same function as COMBINED DOWN method, but moves both elevators instead of upper, then lower
+    public void bothElevatorsDown() {
+        if(getUpperHeight() <= MIN_UPPER_HEIGHT) {
+            setUpperPower(0.0); 
+        } else if (getUpperHeight() < NEAR_MIN_UPPER_HEIGHT) {
+            setUpperPower(-0.3);
+        } else {
+            setUpperPower(-1.0);
+        }
+        if (getLowerHeight() <= MIN_LOWER_HEIGHT) {
+            setLowerPower(0.0);
+            hover();
+        } else if (getLowerHeight() < VERY_CLOSE_MIN_LOWER_HEIGHT) {
+            setLowerPower(-0.1);
+        } else if (getLowerHeight() < NEAR_MIN_LOWER_HEIGHT) {
+            
+        } else {
+            setLowerPower(-1.0);
+        }
+    }
+
+
+    public void upperNeutral() {
+        if (upperStopTargeting == true) {
+            System.out.println("Setting upper to neutral");
+            Robot.elevator.setUpperPower(0.0);
+            targetPosition = getUpperHeight() + getLowerHeight();
+            upperStopTargeting = false;
+        }
+    }
+
 
     /**
     * Move the elevator motor a set number of inches.
     **/
-    public void moveElevatorDistance(double distance) {
-        m_elevatorMotor.set(ControlMode.Position, distance / DIST_PER_PULSE);
+    public void moveLowerDistance(double distance) {
+        m_lowerElevatorMotor.set(ControlMode.Position, distance / DIST_PER_PULSE);
     }
 
     /**
     * Move the gripper motor a set number of inches.
     **/
-    public void moveActuatorDistance(double distance) {
-        m_actuatorMotor.set(ControlMode.Position, distance / DIST_PER_PULSE);
+    public void moveUpperDistance(double distance) {
+        m_upperElevatorMotor.set(ControlMode.Position, distance / DIST_PER_PULSE);
     }
 }
+
