2f4b2bd3ebc22a07c9af6a88eadba0cbba9c8bb3
YardenGoraly <46469577+YardenGoraly@users.noreply.github.com>
2020-01-18 16:08:31 -0800

    This code can generate paths

--- src/main/java/com/team766/frc2019/commands/TurnAround.java
+++ src/main/java/com/team766/frc2019/commands/TurnAround.java
@@ -1,7 +1,11 @@
 package com.team766.frc2019.commands;
 
+import java.util.ArrayList;
+
 import com.team766.framework.Subroutine;
 import com.team766.frc2019.Robot;
+import com.team766.frc2019.Autonomous.GeneratePath;
+import com.team766.frc2019.Autonomous.GeneratePath.Waypoint;
 import com.team766.frc2019.mechanisms.LimeLightI;
 //import com.team766.hal.RobotProvider;
 import com.team766.hal.RobotProvider;
@@ -15,7 +19,17 @@ public class TurnAround extends Subroutine {
     protected void subroutine() {
         System.out.println("TurnAround STARTING");
 
-        callSubroutine(new PreciseTurn((Robot.drive.getGyroAngle() + 180) % 360));
+        ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
+        sWaypoints.add(new Waypoint(116, 209, 0, 0));
+        sWaypoints.add(new Waypoint(105, 225, 0, 30));
+        sWaypoints.add(new Waypoint(5, 2250, 0, 30));
+        sWaypoints.add(new Waypoint(101, 235, 0, 60));
+
+        System.out.println("Waypoints" + sWaypoints);
+
+        System.out.println(GeneratePath.GeneratePath(sWaypoints));
+
+       // callSubroutine(new PreciseTurn((Robot.drive.getGyroAngle() + 180) % 360));
         
     }
 }
