cbc1e4a0ec8e213ece998a80a15db1d09009d10c
grnnja <grnnja@gmail.com>
2020-01-27 20:38:46 -0800

    Add distance calculations for waypoints
    Add method to calculate the total distance each waypoint is from first waypoint is for pathfinding during auton

--- src/main/java/com/team766/frc2019/paths/PathBuilder.java
+++ src/main/java/com/team766/frc2019/paths/PathBuilder.java
@@ -1,12 +1,13 @@
 package com.team766.frc2019.paths;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import com.team766.lib.util.control.Path;
-import com.team766.lib.util.control.PathSegment;
-import com.team766.lib.util.math.RigidTransform2d;
-import com.team766.lib.util.math.Rotation2d;
-import com.team766.lib.util.math.Translation2d;
+// import com.team766.lib.util.control.Path;
+// import com.team766.lib.util.control.PathSegment;
+// import com.team766.lib.util.math.RigidTransform2d;
+// import com.team766.lib.util.math.Rotation2d;
+// import com.team766.lib.util.math.Translation2d;
 
 /**
  * Build paths from a few weaypoints to use with path following
@@ -24,12 +25,14 @@ public class PathBuilder {
     public static ArrayList<Waypoint> buildPath(ArrayList<Waypoint> waypoints) {
         ArrayList<Waypoint> newWaypoints = waypoints;
         newWaypoints = interpolateWaypoints(newWaypoints, spacing);
-        newWaypoints = smoother(newWaypoints, .2, .8, 0.001);
+        newWaypoints = smoother(newWaypoints, .6, .4, 0.001);
+        newWaypoints = calculateDistanceBetweenWaypoints(newWaypoints);
+
         return(newWaypoints);
     }
 
     /**
-     * adds points in between input points with set spacing
+     * adds points in between sequential input points with set spacing
      * used before path smoothing to have enough points to smooth
      * @param waypoints waypoints to interpolate
      * @param spacing spacing between interpolated points
@@ -58,7 +61,7 @@ public class PathBuilder {
     }
 
     /**
-     * Smooths a path of points
+     * Smooths a path of points (order of points determines path)
      * Algorithm reference https://www.youtube.com/watch?v=v0-OUApP_5Q
      * https://www.youtube.com/watch?v=ffLPf8kI6qg
      * @param path
@@ -66,68 +69,98 @@ public class PathBuilder {
      * @param weightSmooth
      * @param tolerance when to stop optimizing (lower value converges slower)
      */
-    public static ArrayList<Waypoint> smoother(ArrayList<Waypoint> path, double weightData, double weightSmooth, double tolerance) {
+    public static ArrayList<Waypoint> smoother(ArrayList<Waypoint> inputPath, double weightData, double weightSmooth, double tolerance) {
         // copy path into 2d array
-        double[][] pathMatrix = new double[path.size()][2];
-        for (int i = 0; i < path.size(); i++) {
-            pathMatrix[i][0] = path.get(i).getX();
-            pathMatrix[i][1] = path.get(i).getY();
+        double[][] path = new double[inputPath.size()][2];
+        for (int i = 0; i < inputPath.size(); i++) {
+            path[i][0] = inputPath.get(i).getX();
+            path[i][1] = inputPath.get(i).getY();
         }
 
-        // copy pathMatrix into newPathMatrix
-        double[][] newPathMatrix = new double[pathMatrix.length][];
+        // copy path into newPath
+        double[][] newPath = new double[path.length][];
 
-        for(int i = 0; i < pathMatrix.length; i++) {
-            newPathMatrix[i] = pathMatrix[i].clone();
+        for(int i = 0; i < path.length; i++) {
+            newPath[i] = path[i].clone();
         }
 
         // gradient descent
         double change = tolerance;
         while(change >= tolerance) {
             change = 0.0;
-            for(int i = 1; i < pathMatrix.length - 1; i++) {
-                for(int j = 0; j < pathMatrix[i].length; j++) {
-                    double aux = newPathMatrix[i][j];
-                    newPathMatrix[i][j] += weightData * (pathMatrix[i][j] - newPathMatrix[i][j]);
-                    newPathMatrix[i][j] += weightSmooth * (newPathMatrix[i-1][j] + newPathMatrix[i+1][j] - (2.0 * newPathMatrix[i][j]));
-                    change += Math.abs(aux - newPathMatrix[i][j]);
+            for(int i = 1; i < path.length - 1; i++) {
+                for(int j = 0; j < path[i].length; j++) {
+                    double aux = newPath[i][j];
+                    newPath[i][j] += weightData * (path[i][j] - newPath[i][j]);
+                    newPath[i][j] += weightSmooth * (newPath[i-1][j] + newPath[i+1][j] - (2.0 * newPath[i][j]));
+                    change += Math.abs(aux - newPath[i][j]);
                 }
             }
         }
 
         // convert 2d array back to ArrayList<Waypoint>
-        ArrayList<Waypoint> newPath = new ArrayList<Waypoint>();
+        ArrayList<Waypoint> outputPath = new ArrayList<Waypoint>();
 
-        for (int i = 0; i < newPathMatrix.length; i++) {
-            newPath.add(new Waypoint(newPathMatrix[i][0], newPathMatrix[i][1], 0, 0));
+        for (int i = 0; i < newPath.length; i++) {
+            outputPath.add(new Waypoint(newPath[i][0], newPath[i][1], 0, 0));
         }
 
-        return newPath;
+        return outputPath;
     }
 
-    private static Waypoint getPoint(ArrayList<Waypoint> w, int i) {
-        if (i > w.size())
-            return w.get(w.size() - 1);
-        return w.get(i);
+    /**
+     * adds distances to totalDistanceFromFirstWaypoint variable
+     * to each waypoint except the last one
+     * @param inputPath
+     * @return
+     */
+    public static ArrayList<Waypoint> calculateDistanceBetweenWaypoints(ArrayList<Waypoint> inputPath) {
+        // copy input array into output array
+        ArrayList<Waypoint> outputPath = new ArrayList<Waypoint>();
+        Iterator<Waypoint> iterator = inputPath.iterator();
+        while(iterator.hasNext()){
+            outputPath.add((Waypoint)(iterator.next()).clone());
+        }
+
+        // acutal distance calculations
+        outputPath.get(0).setTotalDistanceFromFirstWaypoint(0);
+        for (int i = 1; i < inputPath.size(); i++) {
+        // distance at point i = distance at point (i − 1) + distance_formula(point i, point (i − 1))
+            outputPath.get(i).setTotalDistanceFromFirstWaypoint(
+                outputPath.get(i - 1).getTotalDistanceFromFirstWaypoint() +
+                Math.sqrt(
+                    Math.pow(outputPath.get(i).getX() - outputPath.get(i - 1).getX(), 2) +
+                    Math.pow(outputPath.get(i).getY() - outputPath.get(i - 1).getY(), 2)
+                )
+            );
+        }
+
+        return outputPath;
     }
+
+    // private static Waypoint getPoint(ArrayList<Waypoint> w, int i) {
+    //     if (i > w.size())
+    //         return w.get(w.size() - 1);
+    //     return w.get(i);
+    // }
 
     public static class Vector {
-        static double x;
-        static double y;
+        private double x;
+        private double y;
         Vector(double x, double y) {
-            this.x = x;
-            this.y = y;
+            this.setX(x);
+            this.setY(y);
         }
         
-        public static double magnitude() {
+        public double magnitude() {
             return(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
         }
 
-        public static Vector normalize() {
+        public Vector normalize() {
             return(new Vector(x / magnitude(), y / magnitude()));
         }
 
-        public static Vector scale(double scalar) {
+        public  Vector scale(double scalar) {
             return(new Vector(x * scalar, y * scalar));
         }
 
@@ -135,8 +168,16 @@ public class PathBuilder {
             return x;
         }
 
+        public void setX(double x) {
+            this.x = x;
+        }
+
         public double getY() {
             return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
         }
     }
 
@@ -149,40 +190,30 @@ public class PathBuilder {
      * @see GeneratePath
      */
     public static class Waypoint {
-        Translation2d position;
-        double x;
-        double y;
+        // Translation2d position;
+        private double x;
+        private double y;
         double radius;
         double speed;
-        String marker;
 
-        public double getX() {
-            return x;
-        }
+        // used for calculating velocity and curvature
+        // must be entered manually
+        private double totalDistanceFromFirstWaypoint;
+        // String marker;
 
-        public double getY() {
-            return y;
-        }
-
-        public void setX(double newX) {
-            x = newX;
-        }
-
-        public void setY(double newY) {
-            y = newY;
-        }
-
-        public Waypoint(Waypoint other) {
-            this(other.position.x(), other.position.y(), other.radius, other.speed, other.marker);
-        }
+        // public Waypoint(Waypoint other) {
+        //     this(other.position.x(), other.position.y(), other.radius, other.speed, other.marker);
+        // }
 
         public Waypoint(double x, double y, double r, double s) {
             // position = new Translation2d(x, y);
-            this.x = x;
-            this.y = y;
+            this.setX(x);
+            this.setY(y);
             radius = r;
             speed = s;
         }
+
+        public Waypoint() {};
 
         // public Waypoint(Translation2d pos, double r, double s) {
         //     position = pos;
@@ -190,85 +221,118 @@ public class PathBuilder {
         //     speed = s;
         // }
 
-        public Waypoint(double x, double y, double r, double s, String m) {
-            // position = new Translation2d(x, y);
+        // public Waypoint(double x, double y, double r, double s, String m) {
+        //     // position = new Translation2d(x, y);
+        //     this.x = x;
+        //     this.y = y;
+        //     radius = r;
+        //     speed = s;
+        //     // marker = m;
+        // }
+
+        public Waypoint clone(){
+            Waypoint waypoint = new Waypoint();
+
+            waypoint.setX(this.getX());
+            waypoint.setY(this.getY());
+            waypoint.setTotalDistanceFromFirstWaypoint(this.getTotalDistanceFromFirstWaypoint());
+            return waypoint;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
             this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
             this.y = y;
-            radius = r;
-            speed = s;
-            marker = m;
+        }
+
+        public double getTotalDistanceFromFirstWaypoint() {
+            return this.totalDistanceFromFirstWaypoint;
+        }
+
+        public void setTotalDistanceFromFirstWaypoint(double totalDistanceFromFirstWaypoint) {
+            this.totalDistanceFromFirstWaypoint = totalDistanceFromFirstWaypoint;
         }
     }
 
-    /**
-     * An Arc object is formed by two Lines that share a common Waypoint. Contains a center position, radius, and speed.
-     */
-    static class Arc {
-        Line a;
-        Line b;
-        Translation2d center;
-        double radius;
-        double speed;
+//     /**
+//      * An Arc object is formed by two Lines that share a common Waypoint. Contains a center position, radius, and speed.
+//      */
+//     static class Arc {
+//         Line a;
+//         Line b;
+//         Translation2d center;
+//         double radius;
+//         double speed;
 
-        public Arc(Waypoint a, Waypoint b, Waypoint c) {
-            this(new Line(a, b), new Line(b, c));
-        }
+//         public Arc(Waypoint a, Waypoint b, Waypoint c) {
+//             this(new Line(a, b), new Line(b, c));
+//         }
 
-        public Arc(Line a, Line b) {
-            this.a = a;
-            this.b = b;
-            this.speed = (a.speed + b.speed) / 2;
-            this.center = intersect(a, b);
-            this.radius = new Translation2d(center, a.end).norm();
-        }
+//         public Arc(Line a, Line b) {
+//             this.a = a;
+//             this.b = b;
+//             this.speed = (a.speed + b.speed) / 2;
+//             this.center = intersect(a, b);
+//             this.radius = new Translation2d(center, a.end).norm();
+//         }
 
-        private void addToPath(Path p) {
-            a.addToPath(p, speed);
-            if (radius > kEpsilon && radius < kReallyBigNumber) {
-                p.addSegment(new PathSegment(a.end.x(), a.end.y(), b.start.x(), b.start.y(), center.x(), center.y(),
-                        speed, p.getLastMotionState(), b.speed));
-            }
-        }
+//         private void addToPath(Path p) {
+//             a.addToPath(p, speed);
+//             if (radius > kEpsilon && radius < kReallyBigNumber) {
+//                 p.addSegment(new PathSegment(a.end.x(), a.end.y(), b.start.x(), b.start.y(), center.x(), center.y(),
+//                         speed, p.getLastMotionState(), b.speed));
+//             }
+//         }
 
-        private static Translation2d intersect(Line l1, Line l2) {
-            final RigidTransform2d lineA = new RigidTransform2d(l1.end, new Rotation2d(l1.slope, true).normal());
-            final RigidTransform2d lineB = new RigidTransform2d(l2.start, new Rotation2d(l2.slope, true).normal());
-            return lineA.intersection(lineB);
-        }
-    }
+//         private static Translation2d intersect(Line l1, Line l2) {
+//             final RigidTransform2d lineA = new RigidTransform2d(l1.end, new Rotation2d(l1.slope, true).normal());
+//             final RigidTransform2d lineB = new RigidTransform2d(l2.start, new Rotation2d(l2.slope, true).normal());
+//             return lineA.intersection(lineB);
+//         }
+//     }
 
-    /**
-     * A Line object is formed by two Waypoints. Contains a start and end position, slope, and speed.
-     */
-    static class Line {
-        Waypoint a;
-        Waypoint b;
-        Translation2d start;
-        Translation2d end;
-        Translation2d slope;
-        double speed;
+//     /**
+//      * A Line object is formed by two Waypoints. Contains a start and end position, slope, and speed.
+//      */
+//     static class Line {
+//         Waypoint a;
+//         Waypoint b;
+//         Translation2d start;
+//         Translation2d end;
+//         Translation2d slope;
+//         double speed;
 
-        public Line(Waypoint a, Waypoint b) {
-            this.a = a;
-            this.b = b;
-            slope = new Translation2d(a.position, b.position);
-            speed = b.speed;
-            start = a.position.translateBy(slope.scale(a.radius / slope.norm()));
-            end = b.position.translateBy(slope.scale(-b.radius / slope.norm()));
-        }
+//         public Line(Waypoint a, Waypoint b) {
+//             this.a = a;
+//             this.b = b;
+//             slope = new Translation2d(a.position, b.position);
+//             speed = b.speed;
+//             start = a.position.translateBy(slope.scale(a.radius / slope.norm()));
+//             end = b.position.translateBy(slope.scale(-b.radius / slope.norm()));
+//         }
 
-        private void addToPath(Path p, double endSpeed) {
-            double pathLength = new Translation2d(end, start).norm();
-            if (pathLength > kEpsilon) {
-                if (b.marker != null) {
-                    p.addSegment(new PathSegment(start.x(), start.y(), end.x(), end.y(), b.speed,
-                            p.getLastMotionState(), endSpeed, b.marker));
-                } else {
-                    p.addSegment(new PathSegment(start.x(), start.y(), end.x(), end.y(), b.speed,
-                            p.getLastMotionState(), endSpeed));
-                }
-            }
+//         private void addToPath(Path p, double endSpeed) {
+//             double pathLength = new Translation2d(end, start).norm();
+//             if (pathLength > kEpsilon) {
+//                 if (b.marker != null) {
+//                     p.addSegment(new PathSegment(start.x(), start.y(), end.x(), end.y(), b.speed,
+//                             p.getLastMotionState(), endSpeed, b.marker));
+//                 } else {
+//                     p.addSegment(new PathSegment(start.x(), start.y(), end.x(), end.y(), b.speed,
+//                             p.getLastMotionState(), endSpeed));
+//                 }
+//             }
 
-        }
-    }
+//         }
+//     }
 }
