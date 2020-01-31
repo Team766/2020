658b8510d0253590d9faa03e443aed6a152db117
grnnja <grnnja@gmail.com>
2020-01-30 17:43:54 -0800

    Finish pathbuilder and reorganize classes
    Co-Authored-By: samir-rashid <samir-rashid@users.noreply.github.com>
    Co-Authored-By: yardengoraly <yardengoraly@users.noreply.github.com>

---
+++ src/main/java/com/team766/frc2019/paths/Waypoint.java
@@ -0,0 +1,84 @@
+package com.team766.frc2019.paths;
+
+/**
+ * A waypoint along a path. Contains a position, radius (for creating curved
+ * paths), and velocity. The information from these waypoints is used by the
+ * PathBuilder class to generate Paths. Waypoints also contain an optional
+ * marker that is used by the WaitForPathMarkerAction.
+ *
+ * @see GeneratePath
+ */
+public class Waypoint {
+    // Translation2d position;
+    private double x;
+    private double y;
+    private double curvature;
+    // double radius;
+    // double velocity;
+
+    private double velocity;
+
+    // used for calculating velocity and curvature
+    // must be entered manually
+    private double totalDistanceFromFirstWaypoint;
+
+    public Waypoint() {};
+
+    public Waypoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.curvature = 0;
+        this.velocity = 0;
+    };
+
+    public Waypoint clone(){
+        Waypoint waypoint = new Waypoint();
+
+        waypoint.setX(this.getX());
+        waypoint.setY(this.getY());
+        waypoint.setTotalDistanceFromFirstWaypoint(this.getTotalDistanceFromFirstWaypoint());
+        waypoint.setCurvature(this.getCurvature());
+        waypoint.setVelocity(this.getVelocity());
+        return waypoint;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getTotalDistanceFromFirstWaypoint() {
+        return this.totalDistanceFromFirstWaypoint;
+    }
+
+    public void setTotalDistanceFromFirstWaypoint(double totalDistanceFromFirstWaypoint) {
+        this.totalDistanceFromFirstWaypoint = totalDistanceFromFirstWaypoint;
+    }
+
+    public double getCurvature() {
+        return this.curvature;
+    }
+
+    public void setCurvature(double curvature) {
+        this.curvature = curvature;
+    }
+
+    public double getVelocity() {
+        return this.velocity;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+}
