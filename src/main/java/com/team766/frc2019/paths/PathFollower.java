fa4028e3663fae153280919106e685286b32bf67
grnnja <grnnja@gmail.com>
2020-01-30 17:43:58 -0800

    Create PathFollower.java
    Co-Authored-By: samir-rashid <samir-rashid@users.noreply.github.com>
    Co-Authored-By: yardengoraly <yardengoraly@users.noreply.github.com>

---
+++ src/main/java/com/team766/frc2019/paths/PathFollower.java
@@ -0,0 +1,13 @@
+package com.team766.frc2019.paths;
+
+import java.util.ArrayList;
+
+import com.team766.frc2019.paths.Waypoint;
+
+public class PathFollower {
+    ArrayList<Waypoint> newWayPoints = new ArrayList<Waypoint>();
+
+	public static void followPath(ArrayList<Waypoint> path) {
+	}
+
+}
