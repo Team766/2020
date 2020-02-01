16ba6fc8e939dff5d0c21bab89e61c04684946e4
grnnja <grnnja@gmail.com>
2020-02-01 13:12:07 -0800

    Add findLookaheadPoint in PathFollower

--- src/main/java/com/team766/frc2019/commands/TurnAround.java
+++ src/main/java/com/team766/frc2019/commands/TurnAround.java
@@ -12,7 +12,6 @@ import com.team766.frc2019.paths.Waypoint;
 // import com.team766.hal.RobotProvider;
 
 public class TurnAround extends Subroutine {
-
     public TurnAround() {
         takeControl(Robot.drive);
     }
@@ -21,9 +20,9 @@ public class TurnAround extends Subroutine {
         System.out.println("TurnAround STARTING");
 
         ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
-        waypoints.add(new Waypoint(0, 90));
-        waypoints.add(new Waypoint(45, 0));
-        waypoints.add(new Waypoint(30, -50));
+        waypoints.add(new Waypoint(0, 0));
+        waypoints.add(new Waypoint(90, 0));
+        waypoints.add(new Waypoint(130, -50));
 
         // System.out.println("Waypoints" + waypoints);
 
@@ -36,9 +35,9 @@ public class TurnAround extends Subroutine {
         System.out.println("path built");
 
         for (int i = 0; i < path.size(); i++) {
-            System.out.println("Point " + i + ": " + path.get(i).getX() + " " + path.get(i).getY());
-            Waypoint position = new Waypoint(0, 0);
-            System.out.println("distance to 0,0 " + Waypoint.calculateDistanceBetweenTwoWaypoints(path.get(i), position));
+            System.out.println("(" + path.get(i).getX() + "," + path.get(i).getY() + ")");
+            // Waypoint position = new Waypoint(0, 0);
+            // System.out.println("distance to 0,0 " + Waypoint.calculateDistanceBetweenTwoWaypoints(path.get(i), position));
             // System.out.println("("  + path.get(i).getX() + ", " + path.get(i).getY() + ")");
             // System.out.println("curvature: " + path.get(i).getCurvature());
         }
@@ -47,10 +46,12 @@ public class TurnAround extends Subroutine {
 
         // int closesetPointIndex = pathFollower.findClosestPointIndex(path, 0, Robot.drive.getXPosition(), Robot.drive.getYPosition());
 
-        int closestPointIndex = pathFollower.findClosestPointIndex(path, 0, 0, 0);
+        // int closestPointIndex = pathFollower.findClosestPointIndex(path, 0, 0, 0);
 
-        System.out.println("closest point index  " + closestPointIndex);
-        System.out.println("x, y " + path.get(closestPointIndex).getX() + ", " + path.get(closestPointIndex).getY());
+        Waypoint lookaheadPoint = pathFollower.findLookaheadPoint(path, 80, 17, 14, 13);
+
+        // System.out.println("closest point index  " + lookaheadPoint);
+        System.out.println("(" + lookaheadPoint.getX() + "," + lookaheadPoint.getY() + ")");
         // System.out.println("Max speeds:");
         // for (int i = 0; i < path.size(); i++) {
         //     // System.out.println("Point " + i + ": " + path.get(i).getX() + " " + path.get(i).getY());
--- src/main/java/com/team766/frc2019/paths/PathBuilder.java
+++ src/main/java/com/team766/frc2019/paths/PathBuilder.java
@@ -220,7 +220,6 @@ public class PathBuilder {
                 )
             );
         }
-
         return outputPath;
     }
 }
