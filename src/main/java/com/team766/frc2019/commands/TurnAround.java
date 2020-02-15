6273e5bd07c65dae92d294122bf683ae14d08275
grnnja <grnnja@gmail.com>
2020-02-15 14:58:58 -0800

    Fix robot not starting and add SmartDashboard to drive code

--- src/main/java/com/team766/frc2019/commands/TurnAround.java
+++ src/main/java/com/team766/frc2019/commands/TurnAround.java
@@ -54,11 +54,9 @@ public class TurnAround extends Subroutine {
 
         PIDController m_turnController = new PIDController(Robot.drive.P, Robot.drive.I, Robot.drive.D, Robot.drive.THRESHOLD, RobotProvider.getTimeProvider());
 
-        boolean isPathDone = false;
-
         m_turnController.setSetpoint(0.0);
         int i = 0;
-        while(!isPathDone) {
+        while(!pathFollower.isPathDone()) {
             if (i % 100 == 0) {
                 // System.out.println("position: " + Robot.drive.getXPosition() + ", " + Robot.drive.getYPosition());
                 System.out.printf("heading %.2f steering error angle %.2f pid output %.2f \n", Robot.drive.getGyroAngle(), pathFollower.calculateSteeringError(), m_turnController.getOutput());
@@ -68,25 +66,19 @@ public class TurnAround extends Subroutine {
             i++;
 
             pathFollower.setPosition(Robot.drive.getXPosition(), Robot.drive.getYPosition());
-            
-            if ((pathFollower.calculateSteeringError() > 90) || (pathFollower.calculateSteeringError() < -90)) {
-                Drive.setInvertStatus(true);
-                Drive.invertMotors();
-            }
-            
-            if (!Drive.getInverted()) { //TODO: Make sure this angle stuff behaves properly (domain of angles and stuff)
-                pathFollower.setHeading(Robot.drive.getGyroAngle());
-            } else {
-                pathFollower.setHeading(Robot.drive.getGyroAngle() + 180);
-            }
-            pathFollower.update();
+            pathFollower.setHeading(Robot.drive.getGyroAngle());
 
-            if (pathFollower.isPathDone()) {
-                Robot.drive.setDrive(0, 0);
-                Drive.resetMotorOrientation();
-                System.out.println("path followed");
-                return;
-            }
+            // if ((pathFollower.calculateSteeringError() > 90) || (pathFollower.calculateSteeringError() < -90)) {
+            //     Drive.setInvertStatus(true);
+            //     Drive.invertMotors();
+            // }
+            
+            // if (!Drive.getInverted()) { //TODO: Make sure this angle stuff behaves properly (domain of angles and stuff)
+            //     pathFollower.setHeading(Robot.drive.getGyroAngle());
+            // } else {
+            //     pathFollower.setHeading(Robot.drive.getGyroAngle() + 180);
+            // }
+            pathFollower.update();
 
             m_turnController.calculate(pathFollower.calculateSteeringError(), true);
             double turnPower = m_turnController.getOutput() * 500;
@@ -94,14 +86,17 @@ public class TurnAround extends Subroutine {
             // System.out.println("closest point index" + findClosestPointIndex());
             double straightPower = path.get(pathFollower.findClosestPointIndex()).getVelocity();
 
-            if (!Drive.getInverted()) { 
-                Robot.drive.setDrive(straightPower + turnPower, straightPower - turnPower);
-            } else {
-                Robot.drive.setDrive(straightPower - turnPower, straightPower + turnPower);
-            }
+            // if (!Drive.getInverted()) { 
+            //     Robot.drive.setDrive(straightPower + turnPower, straightPower - turnPower);
+            // } else {
+            //     Robot.drive.setDrive(straightPower - turnPower, straightPower + turnPower);
+            // }
+            Robot.drive.setDrive(straightPower + turnPower, straightPower - turnPower);
             
             // allow odometry and other stuff to happen
             yield();
         }
+        Robot.drive.setDrive(0, 0);
+        System.out.println("path followed");
     }
 }
