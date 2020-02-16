fddf3a2278633787836c35445eed567f958a8303
17acastelein <alex.castelein@gmail.com>
2020-02-15 21:27:50 -0800

    added some code

--- src/main/java/com/team766/frc2019/mechanisms/Climber.java
+++ src/main/java/com/team766/frc2019/mechanisms/Climber.java
@@ -3,23 +3,29 @@ package com.team766.frc2019.mechanisms;
 import com.team766.framework.Mechanism;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.CANSpeedController;
-
+import com.team766.hal.SolenoidController;
 
 public class Climber extends Mechanism {
 
     // Climber is pneumatics so I'm not sure how that's going to work
-
-    private CANSpeedController m_climber;
+    private SolenoidController m_climbPiston;
+    private CANSpeedController m_winch;
 
     public Climber() {
+        m_climbPiston = RobotProvider.instance.getSolenoid("climber.piston");
+        m_winch = RobotProvider.instance.getTalonCANMotor("climber.winch");
     }
 
     public void extend() {
-        // insert code to extend the motor
+        m_climbPiston.set(true);
     }
 
     public void retract() {
-        // insert code to retract motor
+        m_climbPiston.set(false);
+    }
+
+    public void winchSpeed(double speed){
+        m_winch.set(speed);
     }
 }
 
