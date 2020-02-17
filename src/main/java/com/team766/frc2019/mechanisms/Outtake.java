3851ad5570cd26afd0492723fc5e1eba3bba55f7
17acastelein <alex.castelein@gmail.com>
2020-02-16 17:49:01 -0800

    methods to calculate necessary distance
    one method takes velocity and angle and the other take only velocity and assumes a 45 degree angle

--- src/main/java/com/team766/frc2019/mechanisms/Outtake.java
+++ src/main/java/com/team766/frc2019/mechanisms/Outtake.java
@@ -6,6 +6,7 @@ import com.team766.hal.CANSpeedController;
 import com.team766.controllers.PIDController;
 import com.team766.hal.RobotProvider;
 import com.team766.frc2019.Robot;
+import java.lang.Math;
 
 
 public class Outtake extends Mechanism {
@@ -13,6 +14,7 @@ public class Outtake extends Mechanism {
     private CANSpeedController m_talon1;
     private CANSpeedController m_talon2;
     private PIDController m_velocityController;
+    public double height_meters = 1.69; //vertical distance from shooter to center of top ports
 
     public Outtake() {
         m_talon1 = RobotProvider.instance.getTalonCANMotor("outtake.talon1");
@@ -37,7 +39,25 @@ public class Outtake extends Mechanism {
     public void shootingSpeed(double targetVelocity) {
         //m_velocityController = new PIDController(Robot.drive.P, Robot.drive.I, Robot.drive.D, Robot.drive.THRESHOLD, RobotProvider.getTimeProvider());
         //m_velocityController.calculate(Robot.drive.AngleDifference(Robot.drive.getGyroAngle(), m_targetAngle), false);
-
     }
 
+    public double[] distanceNeeded(double velocity, double angle){
+        double[] distances = new double[2];
+        double acc = -9.80665;
+        double InitialVy = velocity * Math.sin(angle);
+        double[] timesToHeight = {((-InitialVy + Math.sqrt(Math.pow(InitialVy, 2) + 2 * acc * height_meters))/acc), ((-InitialVy - Math.sqrt(Math.pow(InitialVy, 2) + 2 * acc * height_meters))/acc)};
+        distances[0] = timesToHeight[0] * velocity * Math.cos(angle);
+        distances[1] = timesToHeight[1] * velocity * Math.cos(angle);
+        return distances;
+    }
+
+    public double[] distanceNeeded45Degrees(double velocity){
+        double[] distances = new double[2];
+        double acc = -9.80665;
+        double InitialVy = velocity * Math.sin(Math.PI / 2);
+        double[] timesToHeight = {((-InitialVy + Math.sqrt(Math.pow(InitialVy, 2) + 2 * acc * height_meters))/acc), ((-InitialVy - Math.sqrt(Math.pow(InitialVy, 2) + 2 * acc * height_meters))/acc)};
+        distances[0] = timesToHeight[0] * velocity * Math.cos(Math.PI / 2);
+        distances[1] = timesToHeight[1] * velocity * Math.cos(Math.PI / 2);
+        return distances;
+    }
 }
