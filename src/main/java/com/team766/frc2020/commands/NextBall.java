856320db5cc4f74a43f43667a6209160fe751b51
Alexander Youngblood <arghalexander3000@gmail.com>
2020-02-18 01:33:18 -0800

    if you import a mechanism you're doing it wrong

--- src/main/java/com/team766/frc2020/commands/NextBall.java
+++ src/main/java/com/team766/frc2020/commands/NextBall.java
@@ -5,7 +5,6 @@ import com.team766.framework.Subroutine;
 import com.team766.frc2020.Robot;
 import com.team766.frc2020.MotionProfile.Profile;
 import com.team766.frc2020.MotionProfile.main;
-import com.team766.frc2020.mechanisms.WaterWheel;
 import com.team766.hal.RobotProvider;
 import com.team766.frc2020.MotionProfile.TrapProfile;
 
@@ -13,7 +12,6 @@ import com.team766.frc2020.MotionProfile.TrapProfile;
 public class NextBall extends Subroutine {
     public NextBall() {
         //takeControl(Robot.drive);
-
     }
 
     protected void subroutine() {
@@ -26,23 +24,21 @@ public class NextBall extends Subroutine {
 
         double vError;
 
-        WaterWheel waterWheel = new WaterWheel();
-
         PIDController m_turnController = new PIDController(Robot.drive.P, Robot.drive.I, Robot.drive.D, Robot.drive.THRESHOLD, RobotProvider.getTimeProvider());
         m_turnController.setSetpoint(0.0);
 
 
         Profile motionProfile = new TrapProfile(v_max, accel, dist, 0, 0);
 
-        // while(Robot.waterWheel.getWheelPosition() != motionProfile.getDistAtTime(motionProfile.getFinalTime())) {
-        //     currentTime = (Math.abs(waterWheel.getWheelVelocity()) - Math.abs(start_velocity)) / accel;
-        //     vError = waterWheel.getWheelVelocity() - motionProfile.getVelocityAtTime(currentTime);
+        // while(Robot.Robot.waterwheel.getWheelPosition() != motionProfile.getDistAtTime(motionProfile.getFinalTime())) {
+        //     currentTime = (Math.abs(Robot.waterwheel.getWheelVelocity()) - Math.abs(start_velocity)) / accel;
+        //     vError = Robot.waterwheel.getWheelVelocity() - motionProfile.getVelocityAtTime(currentTime);
             
         //     m_turnController.calculate(vError, true);
         //     double velocityDifference = m_turnController.getOutput();
 
-        //     System.out.println("Wheel velocity: " + waterWheel.getWheelVelocity() + " Velocity difference: " + velocityDifference); 
-        //     waterWheel.setWheelVelocity(motionProfile.getVelocityAtTime(currentTime) + velocityDifference);
+        //     System.out.println("Wheel velocity: " + Robot.waterwheel.getWheelVelocity() + " Velocity difference: " + velocityDifference); 
+        //     Robot.waterwheel.setWheelVelocity(motionProfile.getVelocityAtTime(currentTime) + velocityDifference);
             
 
         // }
