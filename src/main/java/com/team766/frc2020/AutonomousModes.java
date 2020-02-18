bbfa0942b4c75029cce438e03b4b1d11e7b538cb
Samir-Rashid <samirsgotmail@gmail.com>
2020-02-17 23:19:53 -0800

    delete turnaround

--- src/main/java/com/team766/frc2020/AutonomousModes.java
+++ src/main/java/com/team766/frc2020/AutonomousModes.java
@@ -1,9 +1,11 @@
 package com.team766.frc2020;
 
 import com.team766.framework.AutonomousCommand;
-import com.team766.frc2020.commands.TurnAround;
+import com.team766.frc2020.commands.PathRunner;
+
 
 public enum AutonomousModes {
     
-    @AutonomousCommand(commandClass = TurnAround.class) TurnAround,
+    @AutonomousCommand(commandClass = PathRunner.class) PathRunner,
+
 }
