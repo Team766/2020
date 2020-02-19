e9ed2dd43d342215b78ae7e2a3cc27a0ae59cada
frenchyalex <apocesar11@gmail.com>
2020-02-18 16:41:28 -0800

    Update AutonomousModes.java

--- src/main/java/com/team766/frc2020/AutonomousModes.java
+++ src/main/java/com/team766/frc2020/AutonomousModes.java
@@ -2,8 +2,10 @@ package com.team766.frc2020;
 
 import com.team766.framework.AutonomousCommand;
 import com.team766.frc2020.commands.TurnAround;
+import com.team766.frc2020.commands.LimeScore;
 
 public enum AutonomousModes {
     
     @AutonomousCommand(commandClass = TurnAround.class) TurnAround,
+    @AutonomousCommand(commandClass = LimeScore.class) LimeScore,
 }
