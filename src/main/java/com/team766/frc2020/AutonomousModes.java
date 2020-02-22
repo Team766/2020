43fe564116b6d52dae344618f2f0f0155714b750
grnnja <grnnja@gmail.com>
2020-02-22 12:42:30 -0800

    comment out code to run on 2019 robot

--- src/main/java/com/team766/frc2020/AutonomousModes.java
+++ src/main/java/com/team766/frc2020/AutonomousModes.java
@@ -5,13 +5,13 @@ import com.team766.frc2020.commands.TurnAround;
 import com.team766.frc2020.commands.PathRunner;
 import com.team766.frc2020.commands.WebSocketTest;
 
-import com.team766.frc2020.commands.NextBall;
+// import com.team766.frc2020.commands.NextBall;
 
 public enum AutonomousModes {
     
     @AutonomousCommand(commandClass = TurnAround.class) TurnAround,
 
-    @AutonomousCommand(commandClass = NextBall.class) NextBall,
+    // @AutonomousCommand(commandClass = NextBall.class) NextBall,
     @AutonomousCommand(commandClass = PathRunner.class) PathRunner,
     @AutonomousCommand(commandClass = WebSocketTest.class) WebSocketTest
 
