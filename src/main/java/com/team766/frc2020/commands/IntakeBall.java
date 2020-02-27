c5826ea75d8eda2f541300e9818d43e509deabf5
Samir-Rashid <samirsgotmail@gmail.com>
2020-02-26 20:01:04 -0800

    Intake auton

--- src/main/java/com/team766/frc2020/commands/IntakeBall.java
+++ src/main/java/com/team766/frc2020/commands/IntakeBall.java
@@ -5,26 +5,22 @@ import com.team766.frc2020.Robot;
 
 
 public class IntakeBall extends Subroutine {
-    public IntakeBall() {
-    }
-
     protected void subroutine() {
         Robot.intake.setIntakeState(true);
         Robot.intake.setIntakePower(1.0);
         Robot.wagon.setWagonPower(1.0);
         if (Robot.ultrasonic.isBallPresent()){
-            // do nothing
+            System.out.println("Ball is present currently in the waterwheel");
         } else {
             Robot.waterwheel.oneTurn();
         }
 
         // retract all this once the trigger is released
 
-        Robot.intake.setIntakeState(false);
-        Robot.intake.setIntakePower(0);
-        Robot.wagon.setWagonPower(0);
-        Robot.waterwheel.oneTurn();
-
+        // Robot.intake.setIntakeState(false);
+        // Robot.intake.setIntakePower(0);
+        // Robot.wagon.setWagonPower(0);
+        // Robot.waterwheel.oneTurn();
     }
 
 }
