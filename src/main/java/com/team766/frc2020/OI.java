de7aadb793e5618cd4baf2b8fe6a1cc2f0581dad
aidantai <aidantai100@gmail.com>
2020-03-02 20:47:16 -0800

    Mechanisms working!!

--- src/main/java/com/team766/frc2020/OI.java
+++ src/main/java/com/team766/frc2020/OI.java
@@ -20,6 +20,7 @@ public class OI extends Command {
 	private JoystickReader m_joystick2;
 	private JoystickReader m_boxop;
 
+	// Variables for arcade drive
 	private double fwd_power = 0;
 	private double turn_power = 0;
 
@@ -39,32 +40,35 @@ public class OI extends Command {
 	
 	public void run() {
 
-		if (Math.abs(m_joystick1.getRawAxis(1)) < 0.13 ) {
-			fwd_power = 0;
-		} else {
-			fwd_power = -(0.05*(Math.abs(m_joystick1.getRawAxis(1))/m_joystick1.getRawAxis(1)) + Math.pow(m_joystick1.getRawAxis(1), 3));
-		}
-		if (Math.abs(m_joystick2.getRawAxis(0)) < 0.13 ) {
-			turn_power = 0;
-		} else {
-			turn_power = (Math.abs(m_joystick2.getRawAxis(0))*1.3/m_joystick2.getRawAxis(0)) + Math.pow(m_joystick2.getRawAxis(0), 3);
-			turn_power = 0.20 * turn_power;
-			if (Math.abs(fwd_power) > 0.5) {
-				turn_power = 0.6 * turn_power;
-			}
-		}
+		// if (Math.abs(m_joystick1.getRawAxis(1)) < 0.13 ) {
+		// 	fwd_power = 0;
+		// } else {
+		// 	fwd_power = -(0.05 + Math.pow(m_joystick1.getRawAxis(1), 3));
+		// }
+		// if (Math.abs(m_joystick2.getRawAxis(0)) < 0.13 ) {
+		// 	turn_power = 0;
+		// } else {
+		// 	turn_power = (1.3 + Math.pow(m_joystick2.getRawAxis(0), 3);
+		// 	turn_power = 0.20 * turn_power;
+		// 	if (Math.abs(fwd_power) > 0.5) {
+		// 		turn_power = 0.6 * turn_power;
+		// 	}
+		// }
 
-		double normalizer = Math.max(Math.abs(fwd_power),Math.abs(turn_power))/(Math.abs(fwd_power) + Math.abs(turn_power)); // divides both motor powers by the larger one to keep the ratio and keep power at or below 1
-		double leftPower = (fwd_power + turn_power);
-		double rightPower = (fwd_power - turn_power);
+		// double normalizer = Math.max(Math.abs(fwd_power),Math.abs(turn_power))/(Math.abs(fwd_power) + Math.abs(turn_power)); // divides both motor powers by the larger one to keep the ratio and keep power at or below 1
+		// double leftPower = (fwd_power + turn_power);
+		// double rightPower = (fwd_power - turn_power);
 		
-		//SmartDashboard.putNumber("Forward Power", fwd_power);
-		//SmartDashboard.putNumber("Turn Power", turn_power);
-		//SmartDashboard.putNumber("Left Power", leftPower);
-		//SmartDashboard.putNumber("Right Power", rightPower);
+		// //SmartDashboard.putNumber("Forward Power", fwd_power);
+		// //SmartDashboard.putNumber("Turn Power", turn_power);
+		// //SmartDashboard.putNumber("Left Power", leftPower);
+		// //SmartDashboard.putNumber("Right Power", rightPower);
 		
-		Robot.drive.setDrive(leftPower, rightPower);
+		// Robot.drive.setDrive(leftPower, rightPower);
+
+		Robot.drive.arcadeDrive(-m_joystick1.getRawAxis(1), m_joystick2.getRawAxis(0));
 		
+		// Climber
 		if (m_boxop.getRawButton(5)) {
             Robot.climber.setClimberUpState(false);
             Robot.climber.setClimberDownState(true);
@@ -110,7 +114,7 @@ public class OI extends Command {
 			Robot.wagon.setWagonPower(0);
 		} else if (m_boxop.getRawButton(19)) {
 			// Robot.spinner.setSpinnerPower(0.5);
-			Robot.wagon.setWagonPower(-0.5); // temporarily map the spinner speed to negative wagon
+			Robot.wagon.setWagonPower(0.5); // temporarily map the spinner speed to negative wagon
 		}
 
 		if (m_boxop.getRawButton(20)) {
@@ -120,9 +124,9 @@ public class OI extends Command {
 		}
 
 		if (m_boxop.getRawButton(22)) {
-			Robot.wagon.setWagonPower(1.0);
+			Robot.waterwheel.setPusherState(true);
 		} else if (m_boxop.getRawButton(23)) {
-			Robot.wagon.setWagonPower(0);
+			Robot.waterwheel.setPusherState(false);
 		}
 
 		if (m_joystick2.getRawButton(1)) {
@@ -131,12 +135,11 @@ public class OI extends Command {
 			Robot.waterwheel.setPusherState(false);
 		}
 
+		Robot.waterwheel.setWheelPower(m_joystick1.getRawAxis(0));
 		if (m_joystick1.getRawButton(2)) {
 			Robot.waterwheel.setWheelPower(-0.3);
 		} else if (m_joystick2.getRawButton(2)) {
 			Robot.waterwheel.setWheelPower(0.3);
-		} else {
-			Robot.waterwheel.setWheelPower(0);
 		}
 		
 		if (!Robot.drive.isEnabled()) {
