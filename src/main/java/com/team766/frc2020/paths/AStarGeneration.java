161c269f53112acc88db20e49a3158d8e2549ed7
Samir-Rashid <samirsgotmail@gmail.com>
2020-06-07 17:04:23 -0700

    1/6 resolutionify map and cool way to print the map

--- src/main/java/com/team766/frc2020/paths/AStarGeneration.java
+++ src/main/java/com/team766/frc2020/paths/AStarGeneration.java
@@ -4,9 +4,30 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Arrays;
 
+import com.team766.frc2020.paths.InputMapArray;
+
     
 public class AStarGeneration {
-    /* // This block of code can iake in any image and will output a text file "byte.map". As the name says, it is in bytes. You can just convert it to whatever other format you want.
+
+     public static void main(String[] args) {
+        //InputMapArray.main();
+        // InputMapArray.ImageBoolArray[y AKA row][x AKA col];
+
+
+
+    /*
+        //make sure no out of bounds collisions and stuff, should be fine
+        //makes a low res 1/6th res map to make calculation faster (1/6 pixel will make each node one 1/2 robot width which is good)
+        for(int i = 0; i < 707; i+6)
+            for(int j = 0; j < 1384; j+6)
+                array.setLowResArray(i, j, image.getRGB(i, j)); 
+    */
+    }
+
+
+
+
+        /* // This block of code can iake in any image and will output a text file "byte.map". As the name says, it is in bytes. You can just convert it to whatever other format you want.
         // I made it into binary to minimize the size and make it easily intakeable as a binary map for passable areas in a boolean array
     public static void main(String[] args) throws IOException{
         File imgPath = new File("src\\main\\java\\com\\team766\\frc2020\\paths\\CLEAN FieldMap.png");
@@ -21,17 +42,4 @@ public class AStarGeneration {
         } catch (IOException e) {
         }
     }  */
-
-     public static void main(String[] args) throws IOException{
-       
-
-
-    /*
-        //make sure no out of bounds collisions and stuff, should be fine
-        //makes a low res 1/6th res map to make calculation faster (1/6 pixel will make each node one 1/2 robot width which is good)
-        for(int i = 0; i < 707; i+6)
-            for(int j = 0; j < 1384; j+6)
-                array.setLowResArray(i, j, image.getRGB(i, j)); 
-    */
-    }
 }
--- src/main/java/com/team766/frc2020/paths/InputMapArray.java
+++ src/main/java/com/team766/frc2020/paths/InputMapArray.java
@@ -4,11 +4,14 @@ import java.io.DataInputStream;
 import java.io.FileInputStream; 
 import java.io.IOException; 
   
-public class Input 
+// This class takes in the binary representation of the passable field map in binary.map and makes it an array in very fancy efficient way
+public class InputMapArray 
 { 
-    public static boolean[][] imageBoolArray = new boolean[1384][707];
     static class Reader 
     { 
+        final private static int width = 707/6;
+        final private static int height = 1384/6;
+        public static boolean[][] imageBoolArray = new boolean[height][width];
         final private int BUFFER_SIZE = 1 << 16; 
         private DataInputStream din; 
         private byte[] buffer; 
@@ -60,24 +63,58 @@ public class Input
             if (din == null) 
                 return; 
             din.close(); 
-        } 
+        }
+        
+        public static void printArray() {
+            for (int i = 0; i <= width; i++) {
+                System.out.print(" _"); // border of map
+            }
+            System.out.print("\n");
+
+            for (int j = 0; j < height; j++) {
+                System.out.print("|"); // border of map
+                for (int i = 0; i < width; i++) {
+                    if (imageBoolArray[j][i]) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print(" #"); // draw unwalkable
+                    }
+                }
+                System.out.print("|\n"); // border of map
+            }
+
+            for (int i = 0; i <= width; i++) {
+                System.out.print(" _"); // border of map
+            }
+        }
   
         public static void main(String[] args) throws IOException 
         { //978488 ints
             Reader s = new Reader(); 
-            for (int i = 0; i < 1384; i++) {
-                for (int j = 0; j < 707; j++) {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
                     if (s.nextInt() == 1) {
                         imageBoolArray[i][j] = true;
                     } else {
                         imageBoolArray[i][j] = false;
                     }
+                    for (int k = 0; k < 5; k++) {
+                        s.nextInt();
+                    }
                 }
+                for (int k = 0; k < 5; k++) {
+                    s.nextInt();
+                } // reach end of row
+                for (int l = 0; l < 5 * 707; l++) {
+                    s.nextInt();
+                } // skip 5 rows
                 // if (i % 13 == 0) {
                 //     System.out.println("reading map " + i * 707 * 100 / 978488 + " percent done");
                 // }
             }
             s.close();
+            // System.out.println(imageBoolArray.toString());
+            printArray();
         } 
     }
 } 
